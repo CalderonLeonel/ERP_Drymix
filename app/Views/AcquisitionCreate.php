b0bfad3ed414ffb407e376501541ad19b43589b8
SebastianClavijoA <cag5000404@est.univalle.edu>
2022-05-15 19:41:39 -0400

    Agregando vistas de listas relacionadas a adquisiciones y solicitud de adquisiciones

---
+++ app/Views/AcquisitionCreate.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Drymix Adquisiciones</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="vendors/feather/feather.css">
+    <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="vendors/typicons/typicons.css">
+    <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+    <link rel="stylesheet" href="js/select.dataTables.min.css">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="css/vertical-layout-light/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="images/favicon.png" />
+</head>
+<body>
+<div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="home-tab">
+                <div class="d-sm-flex align-items-center justify-content-between border-bottom">
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="nav-item">
+                      <a class="nav-link" id="main-tab" data-bs-toggle="tab" href="#" role="tab" aria-selected="false">Adquisciones</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="material-list-tab" data-bs-toggle="tab" href="#" role="tab" aria-selected="false">Lista de Materiales</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="provider-list-tab" data-bs-toggle="tab" href="#" role="tab" aria-selected="false">Lista de Proveedores</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="material-list-tab" data-bs-toggle="tab" href="#" role="tab" aria-selected="false">Registro de Adquisiciones</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="material-list-tab" data-bs-toggle="tab" href="#" role="tab" aria-selected="false">Solicitud de Adquisiciones</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" id="material-list-tab" data-bs-toggle="tab" href="#" role="tab" aria-selected="false">Lista de Solicitudes</a>
+                    </li>
+                  </ul>
+                </div>
+                <div class="tab-content tab-content-basic">
+                  <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview"> 
+                   
+                    <div class="row">
+                      <div class="col-lg-8 d-flex flex-column">
+                          <div class="col-12 col-lg-4 col-lg-12 grid-margin stretch-card">
+                            <div class="card card-rounded">
+                              <div class="card-body">
+                                <div class="d-sm-flex justify-content-between align-items-start">                             
+                                  <div class="row-lg">
+                                  <div class="col-12">
+                                   <h2 class="card-title card-title-dash">Perfil</h2>
+                                   <h5 class="card-subtitle card-subtitle-dash">Nombre Usuario</h5>
+                                   <button type="button" class="btn btn-primary btn-rounded btn-lg text-light">Ver Perfil</button>
+                                  </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>   
+                      </div>
+                      <div class="col-lg-4 d-flex flex-column">
+                          <div class="col-12 col-lg-4 col-lg-12 grid-margin stretch-card">
+                            <div class="card card-rounded bg-info">
+                              <div class="card-body">
+                              <h4 class="card-title card-title-dash text-light">Volver a la Pagina Principal de Adquisiciones</h4>
+                                <div class="d-sm-flex justify-content-between align-items-start">                             
+                                  <div class="row-lg">
+                                  <div class="col-12">
+                                   <a href="#" class="hidden text-light stretched-link"></a>
+                                  </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>   
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-lg-10 d-flex flex-column">
+                        <div class="row flex-grow">
+                          <div class="col-12 grid-margin stretch-card">
+                            <div class="card card-rounded">
+                              <div class="card-body">
+                                <h2 class="card-title">Registrar una adquisición</h2>
+                                 <p class="card-description">
+                                  Agregue una adquisición
+                                  </p>
+                                <form class="forms-sample">
+                                  <div class="form-group">
+                                   <label for="inputAcquisitionName">Nombre Adquisición</label>
+                                   <input type="text" class="form-control" id="inputAcquisitionName" placeholder="Nombre Adquisición">
+                                  </div>
+                                  <div class="form-group">
+                                       <label for="providerSelect">Proveedor</label>
+                                       <select class="form-control" id="providerSelect">
+                                       <option>proveedor1</option>
+                                       <option>proveedor2</option>
+                                       <option>proveedor3</option>
+                                       <option>proveedor4</option>
+                                       <option>proveedor5</option>
+                                     </select>
+                                  </div>
+                                  <div class="form-group">
+                                     <button type="submit" class="btn btn-lg btn-primary text-light">Registrar Adquisión</button>
+                                  </div>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
+                        </div>                    
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+</body>
+</html>
--- app/Views/AcquisitionProviderList.php
+++ app/Views/AcquisitionProviderList.php
@@ -114,7 +114,7 @@
                                   <div>
                                     <h4 class="card-title card-title-dash">Listado de Adquisiciones</h4>
                                    <p class="card-subtitle card-subtitle-dash">Tiene 3 adquisiciones <span class="text-success">completadas</span></p>
-                                   <p class="card-subtitle card-subtitle-dash">Tiene 5 adquisiciones <span class="text-warning"> en curso</span></p>
+                                   <p class="card-subtitle card-subtitle-dash">Tiene 5 adquisiciones <span class="text-warning">en curso</span></p>
                                    <p class="card-subtitle card-subtitle-dash">Tiene 2 adquisiciones <span class="text-primary">pendientes</span></p>
                                    <p class="card-subtitle card-subtitle-dash">Tiene 2 adquisiciones <span class="text-danger">canceladas</span></p>
                                   </div>
@@ -132,116 +132,17 @@
                                               <input type="checkbox" class="form-check-input" aria-checked="false"><i class="input-helper"></i></label>
                                           </div>
                                         </th>
-                                        <th>Proveedor</th>
-                                        <th>Producto</th>
-                                        <th>Status</th>
+                                        <th>ID</th>
+                                        <th>Proveedor</th>                                       
                                       </tr>
                                     </thead>
                                     <tbody>
-                                      <tr>
                                         <td>
-                                          <div class="form-check form-check-flat mt-0">
-                                            <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input" aria-checked="false"><i class="input-helper"></i></label>
-                                          </div>
+                                          <h6>ID</h6>
                                         </td>
                                         <td>
-                                          <div class="d-flex ">
-                                            <div>
-                                              <h6>Brandon Washington</h6>
-                                              <p>Head admin</p>
-                                            </div>
-                                          </div>
+                                          <h6>NombreProveedor</h6>                                        
                                         </td>
-                                        <td>
-                                          <h6>Company name 1</h6>
-                                          <p>company type</p>
-                                        </td>
-                                        <td><div class="badge badge-opacity-warning">In progress</div></td>
-                                      </tr>
-                                      <tr>
-                                        <td>
-                                          <div class="form-check form-check-flat mt-0">
-                                            <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input" aria-checked="false"><i class="input-helper"></i></label>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <div class="d-flex">
-                                            <div>
-                                              <h6>Laura Brooks</h6>
-                                              <p>Head admin</p>
-                                            </div>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <h6>Company name 1</h6>
-                                          <p>company type</p>
-                                        </td>
-                                        <td><div class="badge badge-opacity-warning">In progress</div></td>
-                                      </tr>
-                                      <tr>
-                                        <td>
-                                          <div class="form-check form-check-flat mt-0">
-                                            <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input" aria-checked="false"><i class="input-helper"></i></label>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <div class="d-flex">
-                                            <div>
-                                              <h6>Wayne Murphy</h6>
-                                              <p>Head admin</p>
-                                            </div>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <h6>Company name 1</h6>
-                                          <p>company type</p>
-                                        </td>
-                                        <td><div class="badge badge-opacity-warning">In progress</div></td>
-                                      </tr>
-                                      <tr>
-                                        <td>
-                                          <div class="form-check form-check-flat mt-0">
-                                            <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input" aria-checked="false"><i class="input-helper"></i></label>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <div class="d-flex">
-                                            <div>
-                                              <h6>Matthew Bailey</h6>
-                                              <p>Head admin</p>
-                                            </div>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <h6>Company name 1</h6>
-                                          <p>company type</p>
-                                        </td>
-                                        <td><div class="badge badge-opacity-danger">Pending</div></td>
-                                      </tr>
-                                      <tr>
-                                        <td>
-                                          <div class="form-check form-check-flat mt-0">
-                                            <label class="form-check-label">
-                                            <input type="checkbox" class="form-check-input" aria-checked="false"><i class="input-helper"></i></label>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <div class="d-flex">
-                                            <div>
-                                              <h6>Katherine Butler</h6>
-                                              <p>Head admin</p>
-                                            </div>
-                                          </div>
-                                        </td>
-                                        <td>
-                                          <h6>Company name 1</h6>
-                                          <p>company type</p>
-                                        </td>
-                                        <td><div class="badge badge-opacity-success">Completed</div></td>
                                       </tr>
                                     </tbody>
                                   </table>
