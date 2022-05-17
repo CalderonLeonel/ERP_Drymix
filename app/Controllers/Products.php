8904f483aa601df370ac38938469f6e0043b782b
Brandon Montoya <MGJ2019761@est.univalle.edu>
2022-05-17 00:19:56 -0400

    Correccion Final de la Navegacion

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -16,7 +16,59 @@ class Products extends BaseController{
         return view('SCM/NuevoProducto');
     }
 
-    public function insert()
+    public function update()
+    {
+        return view('SCM/NuevoProducto');
+    }
+
+    public function delete()
+    {
+        return view('SCM/NuevoProducto');
+    }
+
+    public function InsertProduct()
+    {
+        $productModel = new ProductModel();
+
+        $data = [
+            'nameProduct' => $this->request->getVar('nameProduct'),
+            'IdLine'  => $this->request->getVar('lineProduct'),
+            'IdFormat' => $this->request->getVar('formatProduct')
+        ];
+        $productModel->insert($data);
+        //return $this->response->redirect(site_url('/users-list'));
+        return view('SCM/NuevoProducto');
+    }
+
+    public function UpdateProduct()
+    {
+        $productModel = new ProductModel();
+
+        $data = [
+            'nameProduct' => $this->request->getVar('nameProduct'),
+            'IdLine'  => $this->request->getVar('lineProduct'),
+            'IdFormat' => $this->request->getVar('formatProduct')
+        ];
+        $productModel->insert($data);
+        //return $this->response->redirect(site_url('/users-list'));
+        return view('SCM/NuevoProducto');
+    }
+
+    public function DeleteProduct()
+    {
+        $productModel = new ProductModel();
+
+        $data = [
+            'nameProduct' => $this->request->getVar('nameProduct'),
+            'IdLine'  => $this->request->getVar('lineProduct'),
+            'IdFormat' => $this->request->getVar('formatProduct')
+        ];
+        $productModel->insert($data);
+        //return $this->response->redirect(site_url('/users-list'));
+        return view('SCM/NuevoProducto');
+    }
+
+    public function SelectProduct()
     {
         $productModel = new ProductModel();
 
