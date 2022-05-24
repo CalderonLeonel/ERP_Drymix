b2ecfb054590fb8bc001099ed41512c2464a7933
Brandon Montoya <MGJ2019761@est.univalle.edu>
2022-05-24 12:49:46 -0400

    Builder aniadido

--- app/Controllers/LinesController.php
+++ app/Controllers/LinesController.php
@@ -2,7 +2,110 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use App\Models\Lines;
-class LinesController extends Controller{
+use App\Models\LineModel;
+class LinesController extends BaseController{
 
+
+    //Controladores para la tabla Product
+    public function createProduct()
+    {
+        $lineModel = new LineModel();
+
+        $data['lines'] = $lineModel->orderBy('idLine')->findAll();
+        //$formatModel = new FormatModel();
+        //$lineModel = new LineModel();
+        //$idLine = $_POST['idLine'];
+        //$lineList = $this->LineModel->ListLines();
+        //$idFormat = $_POST['idFormat'];
+        //$formatList = $this->FormatModel->ListFormats();
+        //$dataLines ['Lines'] = $lineList;
+        //$dataFormat ['Format'] = $formatList;
+        
+        return view('SCM/NewLine' ,  $data);
+    }
+
+    public function updateProducts()
+    {
+        $lineModel = new LineModel();
+        $id = [
+            'idLine' => $this->request->getVar('idLine')
+        ];
+
+
+        //$data['list'] = $productTypeModel->readProductsTypes();
+        $data['table'] =$lineModel ->readItem($id);
+        return view('SCM/UpdateProduct', $data);
+    }
+
+    public function deleteProducts()
+    {
+        $lineModel = new LineModel();
+
+        $id = [
+            'idModel' => $this->request->getVar('idModel')
+        ];
+        $data = [   
+            'state' => 0
+        ];
+        $lineModel->deleteProduct($id,$data);
+        $lineModel->readProducts();
+        $data['table'] = $lineModel->readProducts();
+        return view('SCM/DeleteProduct');
+    }
+
+    public function InsertProduct()
+    {
+        $lineModel = new LineModel();
+
+        $data = [
+            'productName' => $this->request->getVar('productName'),
+            'IdLine'  => $this->request->getVar('idLine'),
+            'IdFormat' => $this->request->getVar('idFormat')
+        ];
+        $lineModel->createProduct($data);
+        //return $this->response->redirect(site_url('/users-list'));
+        return view('SCM/DashboardSCM');
+    }
+
+    public function UpdateProduct()
+    {
+        $lineModel = new LineModel();
+        $id = ['idProduct' => $this->request->getVar('idProduct')];
+        $data = [
+            'nameProduct' => $this->request->getVar('nameProduct'),
+            'IdLine'  => $this->request->getVar('lineProduct'),
+            'IdFormat' => $this->request->getVar('formatProduct')
+        ];
+        $lineModel->updateProduct($data, $id);
+        //return $this->response->redirect(site_url('/users-list'));
+        return view('SCM/DashboardSCM');
+    }
+
+    public function DeleteProduct()
+    {
+        $lineModel = new LineModel();
+        $id = ['idProduct' => $this->request->getVar('idProduct')];
+        $data = [
+            'nameProduct' => $this->request->getVar('nameProduct'),
+            'IdLine'  => $this->request->getVar('lineProduct'),
+            'IdFormat' => $this->request->getVar('formatProduct')
+        ];
+        $lineModel->deleteProduct($data , $id);
+        //return $this->response->redirect(site_url('/users-list'));
+        return view('SCM/DashboardSCM');
+    }
+
+    public function ReadProduct()
+    {
+        $lineModel = new LineModel();
+
+        $data = [
+            'nameProduct' => $this->request->getVar('nameProduct'),
+            'IdLine'  => $this->request->getVar('lineProduct'),
+            'IdFormat' => $this->request->getVar('formatProduct')
+        ];
+        $lineModel->readProducts($data);
+        //return $this->response->redirect(site_url('/users-list'));
+        return view('SCM/DashboardSCM');
+    }
 }
--- app/Models/LineModel.php
+++ app/Models/LineModel.php
@@ -6,16 +6,25 @@ use CodeIgniter\Model;
 class LineModel extends Model{
     protected $table      = 'Line';
     // Uncomment below if you want add primary key
-    // protected $primaryKey = 'id';
+    protected $primaryKey = 'idLine';
     protected $allowedFields = ['lineName',  'state' ];
     protected $createdField  = 'createDate';
     protected $updatedField  = 'updateDate';
 
+    protected  $db      = \Config\Database::connect();
+    protected  $builder = $db->table('Products');
     public function ListLines($idLines)
     {
-        $this->select('idLine', 'lineName', 'state');
+        $this->builder->select('idLine', 'lineName', 'state');
         $this->from('Line');
         $this->where('idLine',$idLines);
         $this->get();
     }
+
+    public function InsertProduct ()
+    {
+        $this->builder->insert()
+    }
+
+    
 }
